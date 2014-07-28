0f8a5bea92162de0dfcf8e564003dc4b1e72c480
Enrico Urbinati <Enrico Urbinati@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-28 15:32:24 +0000

    Release 1.1: trunk update
    git-svn-id: https://plugins.svn.wordpress.org/responsive-vector-maps/trunk@956088 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/rvm_core.php
+++ includes/rvm_core.php
@@ -215,7 +215,7 @@ function rvm_mb_function( $post ) {
         // generate the shortcode ;
         
         echo '<div id="rvm_shortcode" class="updated"><p>' . __( 'Use following shortcode to display this map whenever you like ( only once per post/sidebar per page ):' , RVM_TEXT_DOMAIN ) . ' <strong>[rvm_map mapid="' . $post->ID .'"]</strong> .</p></div>'  ;
-        echo '<div id="rvm_donation" class="updated"><p>' . __( 'Do you like <strong>RVM</strong> ? Please consider making a donation now' , RVM_TEXT_DOMAIN )   . '<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=info%40responsivemapsplugin%2ecom&lc=IT&item_name=responsive%20Vector%20Maps%20Plugin&item_number=rvm%2dplugin%2dwordpress%2drepo&currency_code=EUR&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted" target="_blank">
+        echo '<div id="rvm_donation" class="updated"><p>' . __( 'Do you like <strong>RVM</strong> ? Please consider making a donation now' , RVM_TEXT_DOMAIN )   . '<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=info%40responsivemapsplugin%2ecom&lc=IT&item_name=responsive%20Vector%20Maps%20Plugin&item_number=rvm%2dplugin%2dwordpress%2dadmin&currency_code=EUR&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted" target="_blank">
 <img style="vertical-align:middle;margin-left:5px;" src="' . RVM_IMG_PLUGIN_DIR . '/donate_button.png" /></a></p></div>' ;
              
     }

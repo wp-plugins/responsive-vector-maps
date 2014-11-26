881ea6eaed6ba656f1c304d56ac0d63cff030614
Enrico Urbinati <Enrico Urbinati@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-26 22:26:43 +0000

    Tag release ver 2.5
    git-svn-id: https://plugins.svn.wordpress.org/responsive-vector-maps/tags/2.5@1033454 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/regions/belgium-regions.php
@@ -0,0 +1,17 @@
+<?php
+
+$regions = array() ;
+// field value, unique code, name 
+// OK
+$regions['brussels'] = array( 'brussels', 'BE-BRU', 'Brussels' ) ;
+$regions['antwerp'] = array( 'antwerp', 'BE-VAN', 'Antwerp' ) ;
+$regions['luxembourg'] = array( 'luxembourg', 'BE-WLX', 'Luxembourg' ) ;
+$regions['walloon-brabant'] = array( 'walloon-brabant', 'BE-WBR', 'Walloon Brabant' ) ;
+$regions['flemish-brabant'] = array( 'flemish-brabant', 'BE-VBR', 'Flemish Brabant' ) ;
+$regions['east-flanders'] = array( 'east-flanders', 'BE-VOV', 'East Flanders' ) ;
+$regions['liege'] = array( 'liege', 'BE-WLG', 'Liege' ) ;
+$regions['limburg'] = array( 'limburg', 'BE-VLI', 'Limburg' ) ;
+$regions['hainaut'] = array( 'hainaut', 'BE-WHT', 'Hainaut' ) ;
+$regions['namur'] = array( 'namur', 'BE-WNA', 'Namur' ) ;
+$regions['west-flanders'] = array( 'west-flanders', 'BE-VWV', 'West Flanders' ) ;
+?>

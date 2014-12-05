6555c33ca49f84f40730158a20afa0de8e6d3217
Enrico Urbinati <Enrico Urbinati@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-05 22:19:32 +0000

    Rel. ver. 2.5.1
    git-svn-id: https://plugins.svn.wordpress.org/responsive-vector-maps/tags/2.5.1@1039121 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/regions/usa-regions.php
@@ -0,0 +1,57 @@
+<?php
+
+$regions = array() ;
+// field value, unique code, name 
+// OK
+$regions['virginia'] = array( 'virginia', 'US-VA', 'Virginia' ) ;
+$regions['pennsylvania'] = array( 'pennsylvania', 'US-PA', 'Pennsylvania' ) ;
+$regions['tennessee'] = array( 'tennessee', 'US-TN', 'Tennessee' ) ;
+$regions['idaho'] = array( 'idaho', 'US-ID', 'Idaho' ) ;
+$regions['nevada'] = array( 'nevada', 'US-NV', 'Nevada' ) ;
+$regions['texas'] = array( 'texas', 'US-TX', 'Texas' ) ;
+$regions['new-hampshire'] = array( 'new-hampshire', 'US-NH', 'New Hampshire' ) ;
+$regions['new-york'] = array( 'new-york', 'US-NY', 'New York' ) ;
+$regions['hawaii'] = array( 'hawaii', 'US-HI', 'Hawaii' ) ;
+$regions['vermont'] = array( 'vermont', 'US-VT', 'Vermont' ) ;
+$regions['new-mexico'] = array( 'new-mexico', 'US-NM', 'New Mexico' ) ;
+$regions['north-carolina'] = array( 'north-carolina', 'US-NC', 'North Carolina' ) ;
+$regions['north-dakota'] = array( 'north-dakota', 'US-ND', 'North Dakota' ) ;
+$regions['nebraska'] = array( 'nebraska', 'US-NE', 'Nebraska' ) ;
+$regions['louisiana'] = array( 'louisiana', 'US-LA', 'Louisiana' ) ;
+$regions['south-dakota'] = array( 'south-dakota', 'US-SD', 'South Dakota' ) ;
+$regions['district-of-columbia'] = array( 'district-of-columbia', 'US-DC', 'District of Columbia' ) ;
+$regions['delaware'] = array( 'delaware', 'US-DE', 'Delaware' ) ;
+$regions['florida'] = array( 'florida', 'US-FL', 'Florida' ) ;
+$regions['washington'] = array( 'washington', 'US-WA', 'Washington' ) ;
+$regions['kansas'] = array( 'kansas', 'US-KS', 'Kansas' ) ;
+$regions['wisconsin'] = array( 'wisconsin', 'US-WI', 'Wisconsin' ) ;
+$regions['oregon'] = array( 'oregon', 'US-OR', 'Oregon' ) ;
+$regions['kentucky'] = array( 'kentucky', 'US-KY', 'Kentucky' ) ;
+$regions['colorado'] = array( 'colorado', 'US-CO', 'Colorado' ) ;
+$regions['ohio'] = array( 'ohio', 'US-OH', 'Ohio' ) ;
+$regions['oklahoma'] = array( 'oklahoma', 'US-OK', 'Oklahoma' ) ;
+$regions['west-virginia'] = array( 'west-virginia', 'US-WV', 'West Virginia' ) ;
+$regions['wyoming'] = array( 'wyoming', 'US-WY', 'Wyoming' ) ;
+$regions['utah'] = array( 'utah', 'US-UT', 'Utah' ) ;
+$regions['indiana'] = array( 'indiana', 'US-IN', 'Indiana' ) ;
+$regions['illinois'] = array( 'illinois', 'US-IL', 'Illinois' ) ;
+$regions['alaska'] = array( 'alaska', 'US-AK', 'Alaska' ) ;
+$regions['new-jersey'] = array( 'new-jersey', 'US-NJ', 'New Jersey' ) ;
+$regions['maine'] = array( 'maine', 'US-ME', 'Maine' ) ;
+$regions['maryland'] = array( 'maryland', 'US-MD', 'Maryland' ) ;
+$regions['arkansas'] = array( 'arkansas', 'US-AR', 'Arkansas' ) ;
+$regions['massachusetts'] = array( 'massachusetts', 'US-MA', 'Massachusetts' ) ;
+$regions['alabama'] = array( 'alabama', 'US-AL', 'Alabama' ) ;
+$regions['missouri'] = array( 'missouri', 'US-MO', 'Missouri' ) ;
+$regions['minnesota'] = array( 'minnesota', 'US-MN', 'Minnesota' ) ;
+$regions['california'] = array( 'california', 'US-CA', 'California' ) ;
+$regions['iowa'] = array( 'iowa', 'US-IA', 'Iowa' ) ;
+$regions['michigan'] = array( 'michigan', 'US-MI', 'Michigan' ) ;
+$regions['georgia'] = array( 'georgia', 'US-GA', 'Georgia' ) ;
+$regions['arizona'] = array( 'arizona', 'US-AZ', 'Arizona' ) ;
+$regions['montana'] = array( 'montana', 'US-MT', 'Montana' ) ;
+$regions['mississippi'] = array( 'mississippi', 'US-MS', 'Mississippi' ) ;
+$regions['south-carolina'] = array( 'south-carolina', 'US-SC', 'South Carolina' ) ;
+$regions['rhode-island'] = array( 'rhode-island', 'US-RI', 'Rhode Island' ) ;
+$regions['connecticut'] = array( 'connecticut', 'US-CT', 'Connecticut' ) ;
+?>

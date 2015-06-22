1ae3e65417120ede6c47eb6b36e819486928da10
Enrico Urbinati <Enrico Urbinati@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-22 14:00:05 +0000

    Released ver 3.1
    git-svn-id: https://plugins.svn.wordpress.org/responsive-vector-maps/tags/3.1@1185545 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/regions/portugal-regions.php
@@ -0,0 +1,27 @@
+<?php
+    
+$regions = array() ;
+// field value, unique code, name 
+// OK
+$regions['Porto'] = array( 'porto', 'PT-13', 'Porto' ) ;
+$regions['Portalegre'] = array( 'portalegre', 'PT-12', 'Portalegre' ) ;
+$regions['lisboa'] = array( 'lisboa', 'PT-11', 'Lisboa' ) ;
+$regions['leiria'] = array( 'leiria', 'PT-10', 'Leiria' ) ;
+$regions['faro'] = array( 'faro', 'PT-08', 'Faro' ) ;
+$regions['madeira'] = array( 'madeira', 'PT-30', 'Madeira' ) ;
+$regions['azores'] = array( 'azores', 'PT-20', ' Azores' ) ;
+$regions['santarém'] = array( 'santarém', 'PT-14', 'Santar&eacute;m' ) ;
+$regions['bragança'] = array( 'bragança', 'PT-04', 'Bragan&ccedil;a' ) ;
+$regions['castelo-branco'] = array( 'castelo-branco', 'PT-05', 'Castelo Branco' ) ;
+$regions['coimbra'] = array( 'coimbra', 'PT-06', 'Coimbra' ) ;
+$regions['évora'] = array( 'évora', 'PT-07', '&Eacute;vora' ) ;
+$regions['aveiro'] = array( 'aveiro', 'PT-01', 'Aveiro' ) ;
+$regions['beja'] = array( 'beja', 'PT-02', 'Beja' ) ;
+$regions['braga'] = array( 'braga', 'PT-03', 'Braga' ) ;
+$regions['guarda'] = array( 'guarda', 'PT-09', 'Guarda' ) ;
+$regions['viseu'] = array( 'viseu', 'PT-18', 'Viseu' ) ;
+$regions['vila-real'] = array( 'vila-real', 'PT-17', 'Vila Real' ) ;
+$regions['viana-do-castelo'] = array( 'viana-do-castelo', 'PT-16', 'Viana do Castelo' ) ;
+//$regions['setúbal'] = array( 'setúbal', 'PT-15', 'Setúbal' ) ;
+
+?>
---
+++ includes/rvm_download_custom_maps.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * DONATION
+ * ----------------------------------------------------------------------------
+ */
+ 
+    
+$output .= '<div id="rvm_download_custom_maps" class="rvm_new_feature rvm_note" style="position:relative;">
+<div style="width:60px;height:60px;position:absolute;top:2px;left:-6px;background: url(' . RVM_IMG_PLUGIN_DIR . '/new_arrow.png' .') no-repeat 0 0 / 85%;"></div><p><a class="rvm_download_custom_maps_link" href="http://www.responsivemapsplugin.com/expand-your-horizons/" target="_blank"> '
+ . __( 'Download and Install Your Favourite Maps. Extend RVM Capabilities Choosing within a Big Inventory !' , RVM_TEXT_DOMAIN )   .  '
+</a></p></div>' ;
+
+?>
